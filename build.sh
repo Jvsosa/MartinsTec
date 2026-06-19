cf2cedf90196b91abed6d85430d5316ab8df9855
Seu Nome Completo <joaovitorferreiras2002@gmail.com>
2026-06-19 19:17:50 -0300

    Adiciona auto-criação de superusuário no build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --noinput
 
 # Executar migrações do banco de dados
 python manage.py migrate
+
+# Criar superusuário padrão se não existir
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@exemplo.com', 'Admin12345', role='ADMIN')"
